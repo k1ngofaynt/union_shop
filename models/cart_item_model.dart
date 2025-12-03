43bc75312f79705ab3046c4ad7ee12d89002a197
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-03 21:08:25 +0000

    Add CartItem data model

---
+++ models/cart_item_model.dart
@@ -0,0 +1,17 @@
+import 'product.dart';
+
+class CartItemModel {
+  final String id;
+  final Product product;
+  int quantity;
+  String selectedSize;
+  String selectedColor;
+
+  CartItemModel({
+    required this.id,
+    required this.product,
+    required this.quantity,
+    required this.selectedSize,
+    required this.selectedColor,
+  });
+}
