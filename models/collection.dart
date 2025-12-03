6c992c1cc23aac7d791a5cd9f91026b1290acbe2
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-03 21:04:54 +0000

    Add Collection data model

---
+++ models/collection.dart
@@ -0,0 +1,15 @@
+class Collection {
+  final String id;
+  final String name;
+  final String description;
+  final String image;
+  final int productCount;
+
+  Collection({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.productCount,
+  });
+}
