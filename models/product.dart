f6af3c56ccc9e90438e74dee28016cedbbab87e3
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-03 19:43:52 +0000

    updated product data model

--- models/product.dart
+++ models/product.dart
@@ -3,11 +3,23 @@ class Product {
   final String name;
   final String description;
   final double price;
+  final double? originalPrice;
+  final String image;
+  final String collection;
+  final List<String> sizes;
+  final List<String> colors;
+  bool isSale;
   Product({
     required this.id,
     required this.name,
     required this.description,
     required this.price,
-    });
+    this.originalPrice,
+    required this.image,
+    required this.collection,
+    this.sizes = const ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
+    this.colors = const ['Black', 'White', 'Navy'],
+    this.isSale = false,
+  });
+}
   
-  }
