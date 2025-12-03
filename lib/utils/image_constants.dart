28e1a5351189285e22ab4132747a4651cf885d26
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-03 22:05:24 +0000

    Add ImageAssets class

---
+++ lib/utils/image_constants.dart
@@ -0,0 +1,12 @@
+class ImageAssets {
+  
+  static const String productGraduationHoodies =
+      'assets/images/products/product_graduation_hoodies.png';
+  static const String productSignatureHoodie =
+      'assets/images/products/product_signature_hoodie.png';
+  static const String productEssentialTshirt =
+      'assets/images/products/product_essential_tshirt.png';
+  static const String productZipHoodies =
+      'assets/images/products/product_zip_hoodies.png';
+  static const String productCrestedTie =
+      'assets/images/products/product_crested_tie.png';}
