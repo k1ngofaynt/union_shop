43003b5653ac7c3b9a4e4f306a3c8a7e193274c9
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-03 21:16:42 +0000

    Add AppColors utility class

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF2C3E50);
+  static const Color secondary = Color(0xFFF39C12);
+  static const Color accent = Color(0xFFE74C3C);
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color text = Color(0xFF2C3E50);
+  static const Color textLight = Color(0xFF7F8C8D);
+  static const Color border = Color(0xFFECF0F1);
+}
