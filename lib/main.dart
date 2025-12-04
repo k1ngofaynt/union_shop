23d71ac92b5dc760f6e7c45bdc5db9a9ef7e6869
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 15:09:53 +0000

    add main application structure

--- lib/main.dart
+++ lib/main.dart
@@ -8,4 +8,23 @@ import 'package:union_shop/pages/cart_page.dart';
 import 'package:union_shop/widgets/navbar.dart';
 import 'package:union_shop/widgets/footer.dart';
 import 'package:union_shop/utils/constants.dart';
+
 void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Union Shop',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: AppColors.primary,
+          foregroundColor: Colors.white,
+        ),
+      
