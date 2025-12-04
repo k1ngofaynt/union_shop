48e58dd7304eb0bc762f915f92357ec405237344
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 17:09:09 +0000

    add CartPage class

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/utils/constants.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
