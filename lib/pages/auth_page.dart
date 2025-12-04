50454a005501cd654add25e0616fbcefb80e1c8a
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 23:52:38 +0000

    improve layout structure in auth

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -13,4 +13,15 @@ class AuthPage extends StatelessWidget {
         horizontal: isMobile ? 16 : 64,
         vertical: 48,
       ),
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 500),
+          child: Card(
+            elevation: 4,
+            child: Padding(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
 
