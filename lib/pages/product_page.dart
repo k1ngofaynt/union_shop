0b5e5e632ea9067caac23709770c233c2ce8c855
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 19:29:37 +0000

    add ProductPage class

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/models/product.dart';
+import 'package:union_shop/services/product_service.dart';
+import 'package:union_shop/utils/constants.dart';
 
+class ProductPage extends StatefulWidget {
+  final String productId;
+
+  const ProductPage({
+    Key? key,
+    required this.productId,
+  }) : super(key: key);
+
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
