328a653b7f37cd4b1dcb5a518766869d8e798dc4
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 22:57:19 +0000

    add salepage class

---
+++ lib/pages/sale_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/models/product.dart';
+import 'package:union_shop/services/product_service.dart';
+import 'package:union_shop/widgets/product_card.dart';
+
+class SalePage extends StatefulWidget {
+  const SalePage({Key? key}) : super(key: key);
+
+  @override
+  State<SalePage> createState() => _SalePageState();
+}
