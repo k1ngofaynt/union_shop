e5aac1d5c1eec7dcfad517fc4a109195d5291756
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 17:13:03 +0000

    add CartPage state class

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -9,3 +9,23 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  final List<Map<String, dynamic>> cartItems = [
+    {
+      'id': '1',
+      'name': 'Graduation Hoodies',
+      'size': 'L',
+      'color': 'Black',
+      'price': 35.00,
+      'quantity': 1,
+      'image': 'assets/images/product_graduation_hoodies.png',
+    },
+    {
+      'id': '2',
+      'name': 'Essential T-Shirt',
+      'size': 'M',
+      'color': 'White',
+      'price': 10.00,
+      'quantity': 2,
+      'image': 'assets/images/product_essential_tshirt.png',
+    },
+  ];
