961ed968f2da94037bf7b2aa1f31046747c4eeef
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 15:51:01 +0000

    add CollectionsPage class

---
+++ lib/pages/collections_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/models/collection.dart';
+import 'package:union_shop/services/product_service.dart';
+import 'package:union_shop/utils/constants.dart';
+
+class CollectionsPage extends StatefulWidget {
+  const CollectionsPage({Key? key}) : super(key: key);
+
+  @override
+  State<CollectionsPage> createState() => _CollectionsPageState();
+}
