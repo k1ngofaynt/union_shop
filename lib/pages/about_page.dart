d00446a384ec388886e3728c60920c5fe2d4cea9
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 18:19:15 +0000

    add description text

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -26,3 +26,26 @@ class AboutPage extends StatelessWidget {
               ],
             ),
           ),
+          const SizedBox(height: 32),
+          Padding(
+            padding: EdgeInsets.all(isMobile ? 16 : 48),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _ParagraphSection(
+                  content:
+                      'Welcome to the Union Shop!\n\n'
+                      'We\'re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\n'
+                      'All online purchases are available for delivery or instore collection!\n\n'
+                      'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don\'t hesitate to contact us at hello@upsu.net.\n\n'
+                      'Happy shopping!\n\n'
+                      'The Union Shop & Reception Team',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
