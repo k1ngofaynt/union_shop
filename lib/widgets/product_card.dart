c0d0ee2709474ac6fb463a35dd71844fc3807896
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 13:04:09 +0000

    enhance ProductCard widget

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -11,3 +11,33 @@ class ProductCard extends StatelessWidget {
     required this.product,
     required this.onTap,
   }) : super(key: key);
+    @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: AppColors.border),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Image
+            Stack(
+              children: [
+                Container(
+                  color: AppColors.background,
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.network(
+                    product.image,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Container(
+                        color: AppColors.background,
+                        child: const Icon(Icons.image_not_supported),
+                      );
+                    },
+                  ),
+                ),
