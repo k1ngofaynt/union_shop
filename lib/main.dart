f6e12cbe80a3f4ed83b4f060e5bc09e3022e0a5c
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 14:52:22 +0000

    add imports for main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/pages/home_page.dart';
+import 'package:union_shop/pages/about_page.dart';
+import 'package:union_shop/pages/collections_page.dart';
+import 'package:union_shop/pages/collection_detail_page.dart';
 import 'package:union_shop/pages/product_page.dart';
+import 'package:union_shop/pages/cart_page.dart';
 import 'package:union_shop/widgets/navbar.dart';
 import 'package:union_shop/widgets/footer.dart';
+import 'package:union_shop/utils/constants.dart';
 void main() {
   runApp(const UnionShopApp());
 }
