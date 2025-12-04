3e83f23ffd61ed07e90ebd3b24080db7b3d4c829
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 19:33:45 +0000

    add ProductPageState class

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -14,3 +14,21 @@ class ProductPage extends StatefulWidget {
   @override
   State<ProductPage> createState() => _ProductPageState();
 }
+class _ProductPageState extends State<ProductPage> {
+  late Future<Product?> _product;
+  late String _selectedSize;
+  late String _selectedColor;
+  late int _quantity;
+
+  @override
+  void initState() {
+    super.initState();
+    _product = ProductService.getProductById(widget.productId);
+    _selectedSize = 'M';
+    _selectedColor = 'Black';
+    _quantity = 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isMobile = MediaQuery.of(context).size.width < 768;
