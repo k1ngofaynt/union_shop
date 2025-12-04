ba9bdfc1636f077d099df603b3aa06789fe76fe8
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 19:25:38 +0000

    remove  scaffold and header code

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,27 +1,5 @@
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Header
-            Container(
-              height: 100,
-              color: Colors.white,
-              child: Column(
-                children: [
-                  // Top banner
-                  Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    color: const Color(0xFF4d2963),
-                    child: const Text(
-                      'PLACEHOLDER HEADER TEXT',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  ),
+ 
                   // Main header
                   Expanded(
                     child: Container(
