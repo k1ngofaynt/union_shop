fdad6237e85725f899647229ce169ec432420ebe
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 16:57:53 +0000

    add product grid layout

--- lib/pages/collection_detail_page.dart
+++ lib/pages/collection_detail_page.dart
@@ -125,5 +125,14 @@ class _CollectionDetailPageState extends State<CollectionDetailPage> {
                     if (!snapshot.hasData || snapshot.data!.isEmpty) {
                       return const Text('No products in this collection');
                     }
+                     return GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: isMobile ? 2 : 4,
+                        mainAxisSpacing: 16,
+                        crossAxisSpacing: 16,
+                        childAspectRatio: 0.75,
+                      ),
                   
                         
