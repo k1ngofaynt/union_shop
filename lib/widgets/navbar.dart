cd239888a25a0cab33220f19c458e0d8b7343705
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-04 00:03:23 +0000

    search functionality in Navbar widget

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -12,4 +12,23 @@ class Navbar extends StatefulWidget {
   @override
   State<Navbar> createState() => _NavbarState();
 }
+class _NavbarState extends State<Navbar> {
+  late TextEditingController _searchController;
+  bool _isMobileMenuOpen = false;
 
+  @override
+  void initState() {
+    super.initState();
+    _searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isMobile = MediaQuery.of(context).size.width < 768;
+  }}
