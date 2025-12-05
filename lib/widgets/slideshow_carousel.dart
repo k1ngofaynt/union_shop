eb5040719313b4515bed3a0a12c757cda9ac3963
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-05 01:11:48 +0000

    add slideshow class

---
+++ lib/widgets/slideshow_carousel.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SlideshowCarousel extends StatefulWidget {
+  const SlideshowCarousel({Key? key}) : super(key: key);
+
+  @override
+  State<SlideshowCarousel> createState() => _SlideshowCarouselState();
+}
