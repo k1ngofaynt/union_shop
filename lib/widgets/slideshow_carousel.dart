4e0e9ea6254bdd6c7e0a37f05347d76eb2eeb713
k1ngofaynt <yusufalfahad72@gmail.com>
2025-12-05 01:13:06 +0000

    add slides to slideshow carousel

--- lib/widgets/slideshow_carousel.dart
+++ lib/widgets/slideshow_carousel.dart
@@ -12,3 +12,10 @@ class _SlideshowCarouselState extends State<SlideshowCarousel> {
   int _currentPage = 0;
   Timer? _timer;
   bool _isPlaying = true;
+  final List<SlideItem> _slides = [
+    SlideItem(
+      title: 'Essential Range - Over 20% OFF!',
+      description: 'Over 20% off our Essential Range. Come and grab yours while stock lasts!',
+      buttonText: 'BROWSE COLLECTION',
+      backgroundColor: Color(0xFF2B5F44),
+    ),
